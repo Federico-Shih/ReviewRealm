d2b357238f639acc30cfa127307f000861a62a51
fshih <fshih@itba.edu.ar>
2023-06-06 00:46:24 -0300

    fix: remember to drop possible foreign key candidates

--- persistence/src/main/resources/db/migration/V20230605235649__remove-useless-enum-tables.sql
+++ persistence/src/main/resources/db/migration/V20230605235649__remove-useless-enum-tables.sql
@@ -1,5 +1,5 @@
 UPDATE user_roles SET role = r.rolename FROM roles r WHERE r.roleid = user_roles.roleid;
+ALTER TABLE user_roles DROP COLUMN roleid;
 
 UPDATE user_disabled_notifications SET notification = n.notificationtype FROM notifications n WHERE n.notificationid = user_disabled_notifications.notificationid;
-
-
+ALTER TABLE user_disabled_notifications DROP COLUMN notificationid;
