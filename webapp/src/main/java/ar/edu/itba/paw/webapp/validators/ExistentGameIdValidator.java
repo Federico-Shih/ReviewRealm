2022366c4abb2002fd98453a0c91aa500905d151
frupnik <frupnik@itba.edu.ar>
2023-11-29 15:36:28 -0300

    fix: ExistentGameId now takes into account the logged user for suggested games

--- webapp/src/main/java/ar/edu/itba/paw/webapp/validators/ExistentGameIdValidator.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/validators/ExistentGameIdValidator.java
@@ -1,6 +1,9 @@
 package ar.edu.itba.paw.webapp.validators;
 
+import ar.edu.itba.paw.models.User;
 import ar.edu.itba.paw.servicesinterfaces.GameService;
+import ar.edu.itba.paw.servicesinterfaces.UserService;
+import ar.edu.itba.paw.webapp.auth.AuthenticationHelper;
 import ar.edu.itba.paw.webapp.controller.annotations.ExistentGameId;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -9,11 +12,13 @@ import javax.validation.ConstraintValidatorContext;
 
 public class ExistentGameIdValidator implements ConstraintValidator<ExistentGameId, Long> {
     private final GameService gameService;
+    private final UserService userService;
     private boolean optional;
 
     @Autowired
-    public ExistentGameIdValidator(GameService gameService) {
+    public ExistentGameIdValidator(GameService gameService, UserService userService) {
         this.gameService = gameService;
+        this.userService = userService;
     }
 
 
@@ -27,6 +32,8 @@ public class ExistentGameIdValidator implements ConstraintValidator<ExistentGame
     public boolean isValid(Long aLong, ConstraintValidatorContext constraintValidatorContext) {
         if (optional && aLong == null) return true;
         if (aLong == null) return false;
-        return gameService.getGameById(aLong,null).isPresent();
+        User loggedUser = AuthenticationHelper.getLoggedUser(userService);
+        Long userId = loggedUser != null ? loggedUser.getId() : null;
+        return gameService.getGameById(aLong,userId).isPresent();
     }
 }
