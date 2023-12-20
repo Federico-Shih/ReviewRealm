435d0581f01dfd1adf20e29e2db4330b381281ad
Federico Shih <fshih@itba.edu.ar>
2023-12-20 12:56:14 +0000

    fix: incorrect login navigation, fav games breadkcrumbs
    * fix: incorrect login navigation, fav games breadkcrumbs

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,3 +3,7 @@ export const environment = {
   IMAGE_ENDPOINT: 'http://pawserver.it.itba.edu.ar/paw-2023a-04/static/avatars',
 };
 
+// export const environment = {
+//   API_ENDPOINT: 'http://localhost:8080/paw-2023a-04/api',
+//   IMAGE_ENDPOINT: 'http://localhost:8080/paw-2023a-04/static/avatars',
+// };
